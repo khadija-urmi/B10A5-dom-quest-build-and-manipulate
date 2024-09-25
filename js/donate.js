745de58476cf6c8e8505937bbd25a22de0498546
khadija-urmi <khadija5jahan211@gmail.com>
2024-09-25 09:08:32 +0600

    error msg is added

--- js/donate.js
+++ js/donate.js
@@ -1,3 +1,7 @@
+function showErrorMessage(errorMsg, message) {
+    errorMsg.innerText = message;
+    errorMsg.style.display = "block"; // Show the error message
+}
 document.querySelectorAll(".donate-now").forEach(button => {
     button.addEventListener("click", function (event) {
 
@@ -6,9 +10,20 @@ document.querySelectorAll(".donate-now").forEach(button => {
         const currentDonateElement = card.querySelector(".donate-amount");
         const currentDonation = parseFloat(currentDonateElement.innerText);
 
-        //get doantion input 
-        const donateAmountInput = parseFloat(card.querySelector(".donate-input").value);
-        console.log(donateAmountInput);
+        //show error msg
+        const errorMsgDiv = card.querySelector('#error-msg');
+        // Hide the error message initially
+        errorMsgDiv.style.display = "none";
+        errorMsgDiv.innerText = "";
+
+        //get donation input 
+        const donateAmountInputValue = card.querySelector(".donate-input").value;
+
+        const donateAmountInput = parseFloat(donateAmountInputValue);
+        if (donateAmountInputValue === "") {
+            showErrorMessage(errorMsgDiv, "⚠️ Donation amount must be filled out");
+            return;
+        }
 
         //get the current balance
         const currentBalanceElement = document.getElementById("balance");
@@ -18,13 +33,14 @@ document.querySelectorAll(".donate-now").forEach(button => {
 
         //input validation
         if (donateAmountInput <= 0 || isNaN(donateAmountInput)) {
-            alert("Please enter a valid donation amount");
+            showErrorMessage(errorMsgDiv, "⚠️ Please enter a valid donation amount");
             return;
         }
         else if (donateAmountInput > currentBalance || remainingBalance < 0) {
-            alert("Donation amount exceeds the current balance.");
+            showErrorMessage(errorMsgDiv, "⚠️ Donation amount exceeds the current balance.");
             return;
         }
+
         const totalDonation = currentDonation + donateAmountInput;
         currentDonateElement.innerText = totalDonation.toFixed(2);
         currentBalanceElement.innerText = remainingBalance.toFixed(2);
